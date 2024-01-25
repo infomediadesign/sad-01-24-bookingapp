18c5ee8da440bf2987ec0dd2e1a4dfb28539d82d
akshata1203 <akshatanaik1203@gmail.com>
2024-01-25 20:43:07 +0100

    added homelisting feature

--- src/pages/home/Home.js
+++ src/pages/home/Home.js
@@ -2,11 +2,16 @@ import React from 'react'
 import Navbar from '../../components/navbar/Navbar'
 import Header from '../../components/header/Header'
 import Footer from '../../components/footer/Footer'
+import Featured from "../../components/featured/Featured";
+import "./Home.css";
 function Home() {
   return (
     <div>
        <Navbar/>
        <Header/>
+       <div className="homeContainer">
+        <Featured/>
+        </div>
        <Footer/>
     </div>
   )

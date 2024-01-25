ba04abfedd0e739bd4f6776bf91fac57b9d96721
Athul Krishna Ajith Smitha <athulalanchery14@gmail.com>
2024-01-25 21:16:26 +0100

    Initial backend setup

---
+++ backend/server.js
@@ -0,0 +1,30 @@
+import express from "express";
+import dotenv from "dotenv";
+import mongoose from "mongoose";
+const app = express();
+dotenv.config();
+
+const connect = async () => {
+  try {
+    await mongoose.connect(process.env.MONGO, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+    console.log("Connected to MongoDB.");
+  } catch (error) {
+    throw error;
+  }
+};
+
+mongoose.connection.on("disconnected", () => {
+  console.log("MongoDB disconnected!");
+});
+
+mongoose.connection.on("connected", () => {
+  console.log("MongoDB connected!");
+});
+
+app.listen(8000, () => {
+  connect();
+  console.log("Connected to backend!");
+});

bf6b0c227cad56b34ca3ada064ae1f302ae6038f
akshata1203 <akshatanaik1203@gmail.com>
2024-01-25 21:18:02 +0100

    updated homepagelisting

---
+++ src/components/featuredProperties/FeaturedProperties.jsx
@@ -0,0 +1,56 @@
+import "./featuredProperties.css";
+import hotel1 from './hotel1.svg';
+import hotel2 from './hotel2.svg';
+import hotel3 from './hotel3.svg';
+
+const FeaturedProperties = () => {
+  return (
+    <div className="fp">
+      <div className="fpItem">
+        <img
+          src={hotel1}
+          alt="hotel1"
+          className="fpImg"
+        />
+        <span className="fpName">Aparthotel Stare Miasto</span>
+        <span className="fpCity">Madrid</span>
+        <span className="fpPrice">Starting from $120</span>
+        <div className="fpRating">
+          <button>8.9</button>
+          <span>Excellent</span>
+        </div>
+      </div>
+      <div className="fpItem">
+        <img
+          src={hotel2}
+          alt="hotel2"
+          className="fpImg"
+        />
+        <span className="fpName">Comfort Suites Airport</span>
+        <span className="fpCity">Austin</span>
+        <span className="fpPrice">Starting from $140</span>
+        <div className="fpRating">
+          <button>9.3</button>
+          <span>Exceptional</span>
+        </div>
+      </div>
+      <div className="fpItem">
+        <img
+          src={hotel3}
+          alt="hotel3"
+          className="fpImg"
+        />
+        <span className="fpName">Four Seasons Hotel</span>
+        <span className="fpCity">Lisbon</span>
+        <span className="fpPrice">Starting from $99</span>
+        <div className="fpRating">
+          <button>8.8</button>
+          <span>Excellent</span>
+        </div>
+      </div>
+      
+    </div>
+  );
+};
+
+export default FeaturedProperties;
--- src/pages/home/Home.js
+++ src/pages/home/Home.js
@@ -3,6 +3,8 @@ import Navbar from '../../components/navbar/Navbar'
 import Header from '../../components/header/Header'
 import Footer from '../../components/footer/Footer'
 import Featured from "../../components/featured/Featured";
+import FeaturedProperties from "../../components/featuredProperties/FeaturedProperties";
+import PropertyList from "../../components/propertyList/PropertyList";
 import "./Home.css";
 function Home() {
   return (
@@ -11,8 +13,14 @@ function Home() {
        <Header/>
        <div className="homeContainer">
         <Featured/>
+        <h1 className="homeTitle">Browse by property type</h1>
+        <PropertyList/>
+        <h1 className="homeTitle">Homes guests love</h1>
+        <FeaturedProperties/>
+        <Footer/>
+
         </div>
-       <Footer/>
+       
     </div>
   )
 }

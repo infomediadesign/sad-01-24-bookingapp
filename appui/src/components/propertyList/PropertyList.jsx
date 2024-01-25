7597ad915afc40bd5013feb89750e70f18cbbae3
akshata1203 <akshatanaik1203@gmail.com>
2024-01-25 21:18:02 +0100

    updated homepagelisting

---
+++ appui/src/components/propertyList/PropertyList.jsx
@@ -0,0 +1,68 @@
+import "./propertyList.css";
+import hotel1 from './hotel1.svg';
+import hotel2 from './hotel2.svg';
+import hotel3 from './hotel3.svg';
+
+const PropertyList = () => {
+  return (
+    <div className="pList">
+      <div className="pListItem">
+        <img
+           src={hotel1}
+           alt="hotel1"
+          className="pListImg"
+        />
+        <div className="pListTitles">
+          <h1>Hotels</h1>
+          <h2>233 hotels</h2>
+        </div>
+      </div>
+      <div className="pListItem">
+        <img
+           src={hotel2}
+           alt="hotel2"
+          className="pListImg"
+        />
+        <div className="pListTitles">
+          <h1>Apartments</h1>
+          <h2>2331 hotels</h2>
+        </div>
+      </div>
+      <div className="pListItem">
+        <img
+           src={hotel3}
+           alt="hotel3"
+          className="pListImg"
+        />
+        <div className="pListTitles">
+          <h1>Resorts</h1>
+          <h2>2331 hotels</h2>
+        </div>
+      </div>
+      <div className="pListItem">
+        <img
+           src={hotel1}
+           alt="hotel1"
+          className="pListImg"
+        />
+        <div className="pListTitles">
+          <h1>Villas</h1>
+          <h2>2331 hotels</h2>
+        </div>
+      </div>
+      <div className="pListItem">
+        <img
+          src={hotel1}
+          alt="hotel1"
+          className="pListImg"
+        />
+        <div className="pListTitles">
+          <h1>Cabins</h1>
+          <h2>2331 hotels</h2>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default PropertyList;

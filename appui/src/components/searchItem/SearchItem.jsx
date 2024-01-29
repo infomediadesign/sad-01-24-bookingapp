1555d84eead878a60ac77a8c1bdf39a5b2ac0310
EL1M1NAT0R <jcjeffancelvaz@gmail.com>
2024-01-29 19:17:48 +0100

    Made minor variable changes for future modifications

--- appui/src/components/searchItem/SearchItem.jsx
+++ appui/src/components/searchItem/SearchItem.jsx
@@ -12,16 +12,10 @@ const SearchItem = () => {
         <h1 className="siTitle">Tower Street Apartments</h1>
         <span className="siDistance">500m from center</span>
         <span className="siTaxiOp">Free airport taxi</span>
-        <span className="siSubtitle">
-          Studio Apartment with Air conditioning
-        </span>
-        <span className="siFeatures">
-          Entire studio • 1 bathroom • 21m² 1 full bed
-        </span>
-        <span className="siCancelOp">Free cancellation </span>
-        <span className="siCancelOpSubtitle">
-          You can cancel later, so lock in this great price today!
-        </span>
+        <span className="siSubtitle">Studio Apartment with Air conditioning</span>
+        <span className="siFeatures">Entire studio • 1 bathroom • 21m² 1 full bed</span>
+        <span className="siCancelOp">Free cancellation</span>
+        <span className="siCancelOpSubtitle">You can cancel later, so lock in this great price today!</span>
       </div>
       <div className="siDetails">
         <div className="siRating">

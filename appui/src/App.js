a25f4e6ff8424255d1e9f78be28bbfb7bece8966
Chinmayee1398 <chinmayeejoshi5@gmail.com>
2024-01-29 18:14:01 +0100

    hotel search page with image scrolling

--- appui/src/App.js
+++ appui/src/App.js
@@ -5,6 +5,7 @@ import {
 } from "react-router-dom";
 import Home from "./pages/home/Home"
 import List from "./pages/list/List"
+import Hotel from "./pages/hotel/Hotel";
 
 function App() {
   return (
@@ -12,6 +13,7 @@ function App() {
       <Routes>
         <Route path ="/" element={<Home/>}></Route>
         <Route path ="/hotels" element={<List/>}></Route>
+        <Route path ="/hotelsearch" element={<Hotel/>}></Route>
         </Routes>   
     </BrowserRouter>
   );

a3f5e0145af215fd257eeb194c61f1ac3d09724d
akshata1203 <akshatanaik1203@gmail.com>
2024-02-01 01:35:06 +0100

    admin features

--- appui/src/pages/adminhome/AdminHome.jsx
+++ appui/src/pages/adminhome/AdminHome.jsx
@@ -1,15 +1,28 @@
 import AdminSidebar from "../../components/adminsidebar/AdminSidebar";
 import AdminNavbar from "../../components/adminavbar/AdminNavbar";
 import "./adminhome.scss";
+import AdminWidget from "../../components/adminwidget/AdminWidget";
+import AdminFeatured from "../../components/adminfeatured/AdminFeatured";
+import AdminChart from "../../components/adminchart/AdminChart";
 
 const Home = () => {
   return (
     <div className="home">
       <AdminSidebar />
       <div className="homeContainer">
-        <AdminNavbar />
+      <AdminNavbar />
+      <div className="widgets">
+          <AdminWidget type="user" />
+          <AdminWidget type="order" />
+          <AdminWidget type="earning" />
+          <AdminWidget type="balance" />
       </div>
-    </div>
+      <div className="charts">
+          <AdminFeatured />
+          <AdminChart title="Last 6 Months (Revenue)" aspect={2 / 1} />
+        </div>
+      </div>
+    // </div>
   );
 };
 

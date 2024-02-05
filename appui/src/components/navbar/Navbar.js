7a194087863f6e246a7ed76f9952dbe25dfc14b3
Athul Krishna Ajith Smitha <athulalanchery14@gmail.com>
2024-02-05 17:56:13 +0100

    Navbar update with log out button

--- appui/src/components/navbar/Navbar.js
+++ appui/src/components/navbar/Navbar.js
@@ -1,20 +1,39 @@
+
+// export default Navbar;
 import "./navbar.css";
 import { Link } from "react-router-dom";
 import { useContext } from "react";
 import { AuthContext } from "../../context/AuthContext";
+
 const Navbar = () => {
-  const { user } = useContext(AuthContext);
+  const { user, dispatch } = useContext(AuthContext);
+
+  const handleLogout = () => {
+    // Dispatch a LOGOUT action to update the global state
+    dispatch({ type: "LOGOUT" });
+  };
 
   return (
     <div className="navbar">
       <div className="navContainer">
         <Link to="/" style={{ color: "inherit", textDecoration: "none" }}>
-          <span className="logo">BookingApp</span>
+          <span className="logo">Booking App</span>
         </Link>
-        {user ? user.username : (
+        {user ? (
           <div className="navItems">
-            <button className="navButton">Register</button>
-            <button className="navButton">Login</button>
+            <span className="username">{user.username}</span>
+            <button className="navButton" onClick={handleLogout}>
+              Logout
+            </button>
+          </div>
+        ) : (
+          <div className="navItems">
+            <Link to="/signup" style={{ textDecoration: "none" }}>
+              <button className="navButton">Register</button>
+            </Link>
+            <Link to="/login" style={{ textDecoration: "none" }}>
+              <button className="navButton">Login</button>
+            </Link>
           </div>
         )}
       </div>

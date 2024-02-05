acdeb4798247e0d71d4e24540756623930193a9b
akshata1203 <akshatanaik1203@gmail.com>
2024-02-05 23:53:55 +0100

    changed the UI for homepage

--- src/components/navbar/Navbar.js
+++ src/components/navbar/Navbar.js
@@ -1,5 +1,4 @@
 
-// export default Navbar;
 import "./navbar.css";
 import { Link } from "react-router-dom";
 import { useContext } from "react";
@@ -9,33 +8,36 @@ const Navbar = () => {
   const { user, dispatch } = useContext(AuthContext);
 
   const handleLogout = () => {
-    // Dispatch a LOGOUT action to update the global state
     dispatch({ type: "LOGOUT" });
   };
 
   return (
     <div className="navbar">
       <div className="navContainer">
-        <Link to="/" style={{ color: "inherit", textDecoration: "none" }}>
-          <span className="logo">Booking App</span>
-        </Link>
-        {user ? (
-          <div className="navItems">
-            <span className="username">{user.username}</span>
-            <button className="navButton" onClick={handleLogout}>
-              Logout
-            </button>
-          </div>
-        ) : (
-          <div className="navItems">
-            <Link to="/signup" style={{ textDecoration: "none" }}>
-              <button className="navButton">Register</button>
-            </Link>
-            <Link to="/login" style={{ textDecoration: "none" }}>
-              <button className="navButton">Login</button>
-            </Link>
-          </div>
-        )}
+        <div className="logoContainer">
+          <Link to="/" style={{ color: "inherit", textDecoration: "none" }}>
+            <span className="logo"><h1>Booking App</h1></span>
+          </Link>
+        </div>
+        <div className="navItems">
+          {user ? (
+            <>
+              <span className="username">{user.username}</span>
+              <button className="navButton" onClick={handleLogout}>
+                Logout
+              </button>
+            </>
+          ) : (
+            <>
+              <Link to="/signup" style={{ textDecoration: "none" }}>
+                <button className="navButton">Register</button>
+              </Link>
+              <Link to="/login" style={{ textDecoration: "none" }}>
+                <button className="navButton">Login</button>
+              </Link>
+            </>
+          )}
+        </div>
       </div>
     </div>
   );

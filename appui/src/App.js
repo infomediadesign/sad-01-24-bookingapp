e6c8199cd15e523c9b0e1b88f2e04a385ceb5268
Chinmayee1398 <chinmayeejoshi5@gmail.com>
2024-02-06 23:29:20 +0100

    Moving admin files to a different folder

--- appui/src/App.js
+++ appui/src/App.js
@@ -7,12 +7,7 @@ import Home from "./pages/home/Home"
 import List from "./pages/list/List"
 import Hotel from "./pages/hotel/Hotel";
 import Signup from "./pages/signup/Signup";
-import AdminHome from "./pages/adminhome/AdminHome";
 import Login from "./pages/login/Login";
-import AdminList from "./pages/adminlist/AdminList";
-import AdminSingle from "./pages/adminsingle/AdminSingle";
-import AdminNew from "./pages/adminnew/AdminNew";
-import { productInputs, userInputs } from "./formSource";
 
 
 function App() {
@@ -24,23 +19,6 @@ function App() {
         <Route path ="/hotelsearch" element={<Hotel/>}></Route>
         <Route path ="/login" element={<Login/>}></Route>
         <Route path="/signup" element={<Signup/>}/>
-        <Route path ="/adminhome" element={<AdminHome/>}></Route>
-        <Route path="users">
-              <Route index element={<AdminList />} />
-              <Route path=":userId" element={<AdminSingle />} />
-              <Route
-                path="new"
-                element={<AdminNew inputs={userInputs} title="Add New User" />}
-              />
-            </Route>
-            <Route path="products">
-              <Route index element={<AdminList />} />
-              <Route path=":productId" element={<AdminSingle />} />
-              <Route
-                path="new"
-                element={<AdminNew inputs={productInputs} title="Add New Product" />}
-              />
-            </Route>
         </Routes>   
     </BrowserRouter>
   );

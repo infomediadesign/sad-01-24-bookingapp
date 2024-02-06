15683d563f94e88843ad1ae51c1ed609f8d92301
Athul Krishna Ajith Smitha <athulalanchery14@gmail.com>
2024-02-06 13:00:56 +0100

    Api connection resetuped

--- backend/controllers/auth.js
+++ backend/controllers/auth.js
@@ -10,7 +10,7 @@ export const register = async (req, res, next) => {
 
     const newUser = new User({
       ...req.body,
-      password:hash,
+      password: hash,
     });
 
     await newUser.save();
@@ -29,7 +29,7 @@ export const login = async (req, res, next) => {
       user.password
     );
     if (!isPasswordCorrect)
-      return next(createError(400, "Wrong password or username!!!"));
+      return next(createError(400, "Wrong password or username!"));
 
     const token = jwt.sign(
       { id: user._id, isAdmin: user.isAdmin },
--- backend/routes/users.js
+++ backend/routes/users.js
@@ -1,38 +1,36 @@
-import User from "../models/User.js";
+import express from "express";
+import {
+  updateUser,
+  deleteUser,
+  getUser,
+  getUsers,
+} from "../controllers/user.js";
+import { verifyAdmin, verifyToken, verifyUser } from "../utils/verifyToken.js";
 
-export const updateUser = async (req,res,next)=>{
-  try {
-    const updatedUser = await User.findByIdAndUpdate(
-      req.params.id,
-      { $set: req.body },
-      { new: true }
-    );
-    res.status(200).json(updatedUser);
-  } catch (err) {
-    next(err);
-  }
-}
-export const deleteUser = async (req,res,next)=>{
-  try {
-    await User.findByIdAndDelete(req.params.id);
-    res.status(200).json("User has been deleted.");
-  } catch (err) {
-    next(err);
-  }
-}
-export const getUser = async (req,res,next)=>{
-  try {
-    const user = await User.findById(req.params.id);
-    res.status(200).json(user);
-  } catch (err) {
-    next(err);
-  }
-}
-export const getUsers = async (req,res,next)=>{
-  try {
-    const users = await User.find();
-    res.status(200).json(users);
-  } catch (err) {
-    next(err);
-  }
-}
+const router = express.Router();
+
+// router.get("/checkauthentication", verifyToken, (req,res,next)=>{
+//   res.send("hello user, you are logged in")
+// })
+
+// router.get("/checkuser/:id", verifyUser, (req,res,next)=>{
+//   res.send("hello user, you are logged in and you can delete your account")
+// })
+
+// router.get("/checkadmin/:id", verifyAdmin, (req,res,next)=>{
+//   res.send("hello admin, you are logged in and you can delete all accounts")
+// })
+
+//UPDATE
+router.put("/:id", verifyUser, updateUser);
+
+//DELETE
+router.delete("/:id", verifyUser, deleteUser);
+
+//GET
+router.get("/:id", verifyUser, getUser);
+
+//GET ALL
+router.get("/", verifyAdmin, getUsers);
+
+export default router;

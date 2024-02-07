25a65121e59b6e6227e7b4fae54e80465b5bcc0a
akshata1203 <akshatanaik1203@gmail.com>
2024-02-07 04:25:24 +0100

    made changes in the UI

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -4,7 +4,7 @@ import "./home.scss";
 import Widget from "../../components/widget/Widget";
 import Featured from "../../components/featured/Featured";
 import Chart from "../../components/chart/Chart";
-import Table from "../../components/table/Table";
+
 
 const Home = () => {
   return (
@@ -16,16 +16,13 @@ const Home = () => {
           <Widget type="user" />
           <Widget type="order" />
           <Widget type="earning" />
-          <Widget type="balance" />
+         
         </div>
         <div className="charts">
           <Featured />
           <Chart title="Last 6 Months (Revenue)" aspect={2 / 1} />
         </div>
-        <div className="listContainer">
-          <div className="listTitle">Latest Transactions</div>
-          <Table />
-        </div>
+        
       </div>
     </div>
   );
